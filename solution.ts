8f7cc47e975aa8b14d63a0d139a38b93faf1056b
Mahfuj-HSTU <rmahfujur16@gmail.com>
2025-11-16 21:28:23 +0600

    solve problem 1

--- solution.ts
+++ solution.ts
@@ -1,1 +1,10 @@
-// soluiton file
+const formatValue = <T extends string | number | boolean>(value: T): T => {
+  if (typeof value === 'string') {
+    return value.toUpperCase() as T
+  } else if (typeof value === 'number') {
+    return (value * 10) as T
+  } else if (typeof value === 'boolean') {
+    return !value as T
+  }
+  return value
+}
